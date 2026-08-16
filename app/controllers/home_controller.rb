ea9adbb5100d8a0ad53f4a30d4ddf0b3a646ede9
Alana <129349619+prettyalana@users.noreply.github.com>
2026-08-16 10:42:05 -0500

    fix: Add missing lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authenticate, only: [ :show ]
 
   def show
+    Current.player = Player.find_by(token: cookies.encrypted[:auth_token])
     @active_room = Current.player&.room
     if @active_room && (@active_room.round_status == "finished" || @active_room.round_status == "forfeited" || @active_room.round_status == "won")
       cookies.delete(:auth_token)
